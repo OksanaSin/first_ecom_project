3b3c49ef30fb409c3fbfdadb8af82e5c3ba24c69
Ksusha <Sinegubova.ksyusha@mail.ru>
2022-06-08 16:09:32 +0300

    Initial commit

---
+++ books/data.py
@@ -0,0 +1,19 @@
+books = [
+    {
+        "id": "1",
+        "title": "Django for Beginners: Bui",
+        "image_url": "https://images-na.ssl-images-amazon.com/images/I/41-bFTJlrSL._SX384_BO1,204,203,200_.jpg",
+        "price": "30.99",
+        "book_available": "True"
+    },
+
+    {
+        "id": "2",
+        "title": "Django for Professionals:",
+        "image_url": "https://images-na.ssl-images-amazon.com/images/I/61yEnH2STSL.jpg",
+        "price": "40.00",
+        "book_available": "True"
+    },
+
+]
+
